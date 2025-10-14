2fe01f8aeeb50a8bbcfb5356b8eec6f34528f5ae
Lacky227 <tishko.artyr@gmail.com>
2025-10-15 01:30:38 +0300

    feat(authservice): add AuthResponse DTO for authentication responses

---
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.swiftbank.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String role;
+}
