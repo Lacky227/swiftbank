af3c3d819e86699c487b9ef528df80c98c4794ae
Lacky227 <tishko.artyr@gmail.com>
2025-10-15 01:32:21 +0300

    feat(authservice): add builder annotation in User

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/models/User.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/models/User.java
@@ -3,6 +3,7 @@ package com.swiftbank.authservice.models;
 import com.swiftbank.authservice.models.enumModel.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
