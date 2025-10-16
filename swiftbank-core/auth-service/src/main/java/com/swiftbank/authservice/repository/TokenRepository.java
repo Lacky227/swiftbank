b4c61d1e68ce7a1d2fed7396d85edf830508fcf5
Lacky227 <tishko.artyr@gmail.com>
2025-10-16 18:18:40 +0300

    feat(authservice): add TokenRepository

---
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/repository/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.swiftbank.authservice.repository;
+
+import com.swiftbank.authservice.models.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByRefreshToken(String refreshToken);
+}
