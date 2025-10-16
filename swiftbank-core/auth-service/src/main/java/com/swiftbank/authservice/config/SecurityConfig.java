8f9cac2ddf146c031f5899fc8af8d58e41aa3ee3
Lacky227 <tishko.artyr@gmail.com>
2025-10-16 15:25:14 +0300

    feat(authservice): add bean JwtService

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/config/SecurityConfig.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.swiftbank.authservice.config;
 
+import com.swiftbank.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,5 +11,9 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public JwtService jwtService() {
+        return new JwtService();
     }
 }
