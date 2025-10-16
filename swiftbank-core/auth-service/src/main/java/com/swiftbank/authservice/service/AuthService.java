16d2d7c7bdd31ff3e38bc8f74f373bcd177aea80
Lacky227 <tishko.artyr@gmail.com>
2025-10-16 18:17:32 +0300

    feat(authservice): add method refresh

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/service/AuthService.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/service/AuthService.java
@@ -1,14 +1,12 @@
 package com.swiftbank.authservice.service;
 
-import com.swiftbank.authservice.dto.ForgotRequest;
-import com.swiftbank.authservice.dto.LoginRequest;
-import com.swiftbank.authservice.dto.RegisterRequest;
-import com.swiftbank.authservice.dto.ResetRequest;
+import com.swiftbank.authservice.dto.*;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
     ResponseEntity<?> register(RegisterRequest request);
     ResponseEntity<?> login(LoginRequest request);
+    ResponseEntity<?> refresh(RefreshTokenRequest request);
     ResponseEntity<?> forgotPassword(ForgotRequest request);
     ResponseEntity<?> resetPassword(ResetRequest request);
 }
