a54ba1ad3ccdb37ed6ab78e3f65643c9ce9889a0
Lacky227 <tishko.artyr@gmail.com>
2025-10-17 22:19:06 +0300

    refactor(authservice): remove bean JwtService

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/config/SecurityConfig.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.swiftbank.authservice.config;
 
-import com.swiftbank.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,9 +10,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public JwtService jwtService() {
-        return new JwtService();
     }
 }
