9f81083ba99071ba51561afbeb32a0061fa5ffc1
Lacky227 <tishko.artyr@gmail.com>
2025-10-17 22:41:55 +0300

    feat(apigateway): add RouteConfig for auth-service routing

---
+++ swiftbank-core/api-gateway/src/main/java/com/swiftbank/apigateway/config/RouteConfig.java
@@ -0,0 +1,18 @@
+package com.swiftbank.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                        .path("/auth/**")
+                        .uri("http://auth-service:8081/"))
+                .build();
+    }
+}
