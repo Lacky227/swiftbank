4eaa027cab868054b2bcba25d3e18e70753574c5
Lacky227 <tishko.artyr@gmail.com>
2025-10-20 01:13:00 +0300

    feat(notificationservice): add MallingRequest DTO

---
+++ swiftbank-core/notification-service/src/main/java/com/swiftbank/notificationservice/dto/MallingRequest.java
@@ -0,0 +1,14 @@
+package com.swiftbank.notificationservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MallingRequest {
+    private String email;
+    private String subject;
+    private String link;
+}
