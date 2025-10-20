da7810993d2276a7eb627b813fce5452cdae0ff4
Lacky227 <tishko.artyr@gmail.com>
2025-10-20 23:19:31 +0300

    feat(notificationservice): update receiveMessage to request Object

--- swiftbank-core/notification-service/src/main/java/com/swiftbank/notificationservice/service/RabbitMQService.java
+++ swiftbank-core/notification-service/src/main/java/com/swiftbank/notificationservice/service/RabbitMQService.java
@@ -1,6 +1,7 @@
 package com.swiftbank.notificationservice.service;
 
 import com.swiftbank.notificationservice.config.RabbitMQConfig;
+import com.swiftbank.notificationservice.dto.ResetPayload;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ public class RabbitMQService {
     private final SendService sendService;
 
     @RabbitListener(queues = RabbitMQConfig.RESET_QUEUE_NAME)
-    public void receiveMessage(String message) {
-        sendService.forgotPassword(message);
+    public void receiveMessage(Object message) {
+        sendService.forgotPassword((ResetPayload) message);
     }
 }
