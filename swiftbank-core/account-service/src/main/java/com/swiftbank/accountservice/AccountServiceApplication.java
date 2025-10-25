a2c46ac6baad5ffac6e8aabfd804077a50722ebb
Lacky227 <tishko.artyr@gmail.com>
2025-10-25 21:04:42 +0300

    feat: create account-service and add Dockerfile

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbank.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
