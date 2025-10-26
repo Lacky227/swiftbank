b6103a388c831684bc9bbeafc3d0e5852eb80fe4
Lacky227 <tishko.artyr@gmail.com>
2025-10-26 21:28:09 +0200

    feat(accountservice): add RabbitMQ configuration and JSON converter

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package com.swiftbank.accountservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String REGISTER_QUEUE_NAME = "register-queue";
+    public static final String EXCHANGE_NAME = "auth-exchange";
+    public static final String RESET_ROUTING_KEY = "auth.to.account";
+
+    @Bean
+    public Queue registerQueue() {
+        return new Queue(REGISTER_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding registerBinding(Queue resetQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(resetQueue).to(exchange).with(RESET_ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
