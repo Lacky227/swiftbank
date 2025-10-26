70cb128429c94002239582c4b621b0f23e65f5ea
Lacky227 <tishko.artyr@gmail.com>
2025-10-26 21:36:40 +0200

    feat(accountservice): add RabbitMQ service for listener register users and call created account

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/RabbitMQService.java
@@ -0,0 +1,18 @@
+package com.swiftbank.accountservice.service;
+
+import com.swiftbank.accountservice.config.RabbitMQConfig;
+import com.swiftbank.accountservice.dto.UserRegisteredPayload;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RabbitMQService {
+    private AccountService accountService;
+
+    @RabbitListener(queues = RabbitMQConfig.REGISTER_QUEUE_NAME)
+    private void registerAccount(UserRegisteredPayload payload) {
+        accountService.createAccountForNewUser(payload);
+    }
+}
