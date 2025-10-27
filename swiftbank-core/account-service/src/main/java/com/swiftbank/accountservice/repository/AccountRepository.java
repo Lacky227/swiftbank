187d773d5620afeee5e76572b0f32aaf8b9e6901
Lacky227 <tishko.artyr@gmail.com>
2025-10-27 19:58:26 +0200

    feat(accountservice): add AccountRepository

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.swiftbank.accountservice.repository;
+
+import com.swiftbank.accountservice.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    boolean existsAccountByUserId(Long userId);
+    boolean existsAccountByNumber(String number);
+}
