490f4c538fd71a5efc1f2c48a26e1e86036c7af5
Lacky227 <tishko.artyr@gmail.com>
2025-10-27 20:37:57 +0200

    feat(accountservice): add IBANUtils for generate IBAN number

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/utils/IBANUtils.java
@@ -0,0 +1,42 @@
+package com.swiftbank.accountservice.utils;
+
+import com.swiftbank.accountservice.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import lombok.experimental.UtilityClass;
+import org.iban4j.CountryCode;
+import org.iban4j.Iban;
+import org.iban4j.UnsupportedCountryException;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@UtilityClass
+@AllArgsConstructor
+public class IBANUtils {
+    public String generateIBAN(String countryCode, String bankCode, AccountRepository accountRepository) {
+        String accountNumber = generateUniqueAccountNumber(accountRepository);
+        String ibanNumber;
+        try {
+            Iban iban = new Iban.Builder()
+                    .countryCode(CountryCode.valueOf(countryCode))
+                    .bankCode(bankCode)
+                    .accountNumber(accountNumber)
+                    .build();
+            ibanNumber = iban.toString();
+        } catch (UnsupportedCountryException | IllegalArgumentException e) {
+            throw new RuntimeException("Invalid IBAN provided: " + e.getMessage());
+        }
+        return ibanNumber;
+    }
+    private String generateUniqueAccountNumber(AccountRepository accountRepository) {
+        String prefix = "2600";
+        long randomNumber = ThreadLocalRandom.current().nextLong(100_000_000_000_000L);
+        String suffix = String.format("%015d", randomNumber);
+
+        String accountNumber = prefix + suffix;
+
+        if (accountRepository.existsAccountByNumber(accountNumber)) {
+            return generateUniqueAccountNumber(accountRepository);
+        }
+        return accountNumber;
+    }
+}
