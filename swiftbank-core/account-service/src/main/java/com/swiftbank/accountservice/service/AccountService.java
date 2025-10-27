1649b78a1f2566133297efd825b180793df2b2b3
Lacky227 <tishko.artyr@gmail.com>
2025-10-27 19:58:04 +0200

    refactor(accountservice): change type return createAccountForNewUser to void

--- swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/AccountService.java
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/AccountService.java
@@ -3,14 +3,10 @@ package com.swiftbank.accountservice.service;
 import com.swiftbank.accountservice.dto.OperationFundsRequest;
 import com.swiftbank.accountservice.dto.UserRegisteredPayload;
 import com.swiftbank.accountservice.exceptions.InsufficientFundsException;
-import com.swiftbank.accountservice.models.Account;
 import org.springframework.http.ResponseEntity;
 
-import java.math.BigDecimal;
-import java.util.UUID;
-
 public interface AccountService {
-    ResponseEntity<?> createAccountForNewUser(UserRegisteredPayload payload);
+    void createAccountForNewUser(UserRegisteredPayload payload);
     ResponseEntity<?> getAccountsByUserId(Long userId);
     ResponseEntity<?> getBalance(String accountNumber);
     ResponseEntity<?> debitAccount(OperationFundsRequest request) throws InsufficientFundsException;
