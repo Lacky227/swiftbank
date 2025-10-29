dc227f27b36292001f15a77725af130915e41c54
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 15:09:57 +0200

    feat(apigateway): add route and davide route to open and protected

--- swiftbank-core/api-gateway/src/main/java/com/swiftbank/apigateway/config/RouteConfig.java
+++ swiftbank-core/api-gateway/src/main/java/com/swiftbank/apigateway/config/RouteConfig.java
@@ -1,18 +1,30 @@
 package com.swiftbank.apigateway.config;
 
+import com.swiftbank.apigateway.filters.AccessFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@AllArgsConstructor
 public class RouteConfig {
+    private final AccessFilter accessFilter;
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("auth-service", r -> r
+                .route("auth-service-protected", r -> r
+                        .path("/auth/me")
+                        .filters(f -> f.filter(accessFilter))
+                        .uri("lb://auth-service"))
+                .route("auth-service-open", r -> r
                         .path("/auth/**")
                         .uri("lb://auth-service"))
+                .route("account-service", r -> r
+                        .path("/account/user")
+                        .filters(f -> f.filter(accessFilter))
+                        .uri("lb://account-service"))
                 .build();
     }
 }
