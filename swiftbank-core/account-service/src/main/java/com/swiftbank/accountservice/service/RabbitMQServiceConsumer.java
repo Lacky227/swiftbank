2e36abd875fedf99ee9216b9e017fd0e2b0c271f
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 14:35:36 +0200

    refactor(accountservice): rename class and remove send message

--- swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/RabbitMQServiceConsumer.java
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/RabbitMQServiceConsumer.java
@@ -1,28 +1,18 @@
 package com.swiftbank.accountservice.service;
 
 import com.swiftbank.accountservice.config.RabbitMQConfig;
-import com.swiftbank.accountservice.dto.AccountCreatedPayload;
 import com.swiftbank.accountservice.dto.UserRegisteredPayload;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class RabbitMQService {
+public class RabbitMQServiceConsumer {
     private AccountService accountService;
-    private RabbitTemplate rabbitTemplate;
 
     @RabbitListener(queues = RabbitMQConfig.REGISTER_QUEUE_NAME)
     private void registerAccount(UserRegisteredPayload payload) {
         accountService.createAccountForNewUser(payload);
-    }
-
-    public void sendMessage(AccountCreatedPayload payload) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConfig.EXCHANGE_NAME,
-                RabbitMQConfig.CREATED_QUEUE_NAME,
-                payload);
     }
 }
