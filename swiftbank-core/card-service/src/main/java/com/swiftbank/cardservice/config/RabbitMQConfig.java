38baaee20846958d649ef7a93a5380511fe133c2
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 16:36:20 +0200

    feat(cardservice): add RabbitMQ configuration and JSON converter

---
+++ swiftbank-core/card-service/src/main/java/com/swiftbank/cardservice/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package com.swiftbank.cardservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String CREATED_QUEUE_NAME = "created-queue";
+    public static final String EXCHANGE_NAME = "auth-exchange";
+    public static final String CREATED_ROUTING_KEY = "account.to.card";
+
+    @Bean
+    public Queue createdQueue() {
+        return new Queue(CREATED_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding createdBinding(Queue createdQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(createdQueue).to(exchange).with(CREATED_ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
