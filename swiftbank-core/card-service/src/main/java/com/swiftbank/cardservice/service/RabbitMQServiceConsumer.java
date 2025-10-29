8501d819d8e07177fc17b997a013ab73a67fbe6b
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 16:52:46 +0200

    feat(cardservice): add service for listener message

---
+++ swiftbank-core/card-service/src/main/java/com/swiftbank/cardservice/service/RabbitMQServiceConsumer.java
@@ -0,0 +1,17 @@
+package com.swiftbank.cardservice.service;
+
+import com.swiftbank.cardservice.config.RabbitMQConfig;
+import com.swiftbank.cardservice.dto.AccountCreatePayload;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RabbitMQServiceConsumer {
+    private final CardService cardService;
+    @RabbitListener(queues = RabbitMQConfig.CREATED_QUEUE_NAME)
+    private void createdCard(AccountCreatePayload accountCreatePayload) {
+        cardService.createCardForNewAccount(accountCreatePayload);
+    }
+}
