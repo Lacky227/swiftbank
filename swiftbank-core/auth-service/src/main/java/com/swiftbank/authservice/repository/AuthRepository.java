9a92ce9029899f66788111759593a796f3ff41d4
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 15:02:25 +0200

    feat(authservice): add find user by id

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/repository/AuthRepository.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/repository/AuthRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface AuthRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
