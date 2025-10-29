f6ac3be87b9c976e2667ed9d594ca17d25ec9210
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 14:50:04 +0200

    refactor(accountservice): clean createAccountForNewUser and implements getAccountsByUserId, getBalance

--- swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/impl/AccountServiceImpl.java
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/service/impl/AccountServiceImpl.java
@@ -1,30 +1,29 @@
 package com.swiftbank.accountservice.service.impl;
 
-import com.swiftbank.accountservice.dto.AccountCreatedPayload;
-import com.swiftbank.accountservice.dto.OperationFundsRequest;
-import com.swiftbank.accountservice.dto.UserRegisteredPayload;
+import com.swiftbank.accountservice.dto.*;
 import com.swiftbank.accountservice.exceptions.CreateAccountException;
 import com.swiftbank.accountservice.exceptions.InsufficientFundsException;
 import com.swiftbank.accountservice.models.Account;
 import com.swiftbank.accountservice.models.enumModels.Currency;
 import com.swiftbank.accountservice.repository.AccountRepository;
 import com.swiftbank.accountservice.service.AccountService;
-import com.swiftbank.accountservice.service.RabbitMQService;
+import com.swiftbank.accountservice.service.RabbitMQServiceProducer;
 import com.swiftbank.accountservice.utils.IBANUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
-    private final RabbitMQService rabbitMQService;
-    private final PasswordEncoder passwordEncoder;
+    private final RabbitMQServiceProducer rabbitMQServiceProducer;
     @Value("${country.code}")
     private String COUNTRY_CODE;
     @Value("${bank.code}")
@@ -34,7 +33,7 @@ public class AccountServiceImpl implements AccountService {
     public void createAccountForNewUser(UserRegisteredPayload payload) {
         if (
                 payload == null ||
-                payload.getUserId().describeConstable().isEmpty() ||
+                payload.getUserId() == null ||
                 payload.getCreatedAt() == null
         ) {
             throw new CreateAccountException("invalid payload");
@@ -44,8 +43,7 @@ public class AccountServiceImpl implements AccountService {
         }
         Account account = Account.builder()
                 .userId(payload.getUserId())
-                .number(passwordEncoder.encode(
-                        IBANUtils.generateIBAN(COUNTRY_CODE, BANK_CODE, accountRepository)))
+                .number(IBANUtils.generateIBAN(COUNTRY_CODE, BANK_CODE, accountRepository))
                 .balance(BigDecimal.ZERO)
                 .currency(Currency.UAH)
                 .active(true)
@@ -53,21 +51,39 @@ public class AccountServiceImpl implements AccountService {
                 .build();
         accountRepository.save(account);
 
-        rabbitMQService.sendMessage(
+        rabbitMQServiceProducer.sendMessage(
                 AccountCreatedPayload.builder()
                         .accountNumber(account.getNumber())
+                        .userId(account.getUserId())
                         .build()
         );
     }
 
     @Override
     public ResponseEntity<?> getAccountsByUserId(Long userId) {
-        return null;
+        Optional<List<Account>> accountOptional = accountRepository.findAccountsByUserId(userId);
+        if (accountOptional.isEmpty() || accountOptional.get().isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        List<Account> accounts = accountOptional.get();
+        List<AccountResponse> accountResponseList = new ArrayList<>();
+        accounts.forEach(account -> {
+            AccountResponse accountResponse = AccountResponse.builder()
+                    .balance(account.getBalance())
+                    .currency(account.getCurrency().toString())
+                    .build();
+            accountResponseList.add(accountResponse);
+        });
+        return ResponseEntity.ok(accountResponseList);
     }
 
     @Override
-    public ResponseEntity<?> getBalance(String accountNumber) {
-        return null;
+    public ResponseEntity<?> getBalance(GetBalanceRequest request) {
+        Optional<Account> account = accountRepository.findAccountByNumber(request.getAccountNumber());
+        if (account.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(account.get().getBalance());
     }
 
     @Override
