0d5108de972ccb37f358dd534e7969928115bcfd
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 15:01:53 +0200

    refactor(authservice): change param and setSubject in generateToken

--- swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/service/JwtService.java
+++ swiftbank-core/auth-service/src/main/java/com/swiftbank/authservice/service/JwtService.java
@@ -17,9 +17,9 @@ public class JwtService {
     @Value("${jwt.secret-key}")
     private String SECRET_KEY;
 
-    public String generateToken(String email, String role) {
+    public String generateToken(Long userId, String role) {
         return Jwts.builder()
-                .setSubject(email)
+                .setSubject(userId.toString())
                 .claim("role", role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
