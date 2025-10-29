b4c6af2d1afa99d7862a19e3ff7b8047c72032a9
Lacky227 <tishko.artyr@gmail.com>
2025-10-29 14:42:58 +0200

    feat(accountservice): add AccountResponse and GetBalanceRequest DTO

---
+++ swiftbank-core/account-service/src/main/java/com/swiftbank/accountservice/dto/AccountResponse.java
@@ -0,0 +1,15 @@
+package com.swiftbank.accountservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountResponse {
+    private BigDecimal balance;
+    private String currency;
+}
